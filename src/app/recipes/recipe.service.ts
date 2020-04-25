94d11e0d358407a39276b3a8e2273ac49a8420ac
Delia <dedoblege@gmail.com>
2020-04-25 14:06:17 +0200

    applying http knowledge

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,20 +8,21 @@ import { Subject } from 'rxjs/Subject'
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>()
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Spaghetti Soygnese',
-      'Some description for the saghetti',
-      'https://cdn6.recetasdeescandalo.com/wp-content/uploads/2019/05/Spaguetti-a-la-bolonesa-una-receta-de-pasta-para-triunfar.jpg',
-      [new Ingredient('Spaghetti', 20), new Ingredient('soja', 20)]
-    ),
-    new Recipe(
-      'Lasagna',
-      'Some description for the lasagna',
-      'https://www.recetasdesbieta.com/wp-content/uploads/2018/10/lasagna-original.-300x169.jpg',
-      [new Ingredient('Pasta', 1), new Ingredient('Tomato', 1)]
-    ),
-  ]
+  private recipes: Recipe[]
+  //  = [
+  //   new Recipe(
+  //     'Spaghetti Soygnese',
+  //     'Some description for the saghetti',
+  //     'https://cdn6.recetasdeescandalo.com/wp-content/uploads/2019/05/Spaguetti-a-la-bolonesa-una-receta-de-pasta-para-triunfar.jpg',
+  //     [new Ingredient('Spaghetti', 20), new Ingredient('soja', 20)]
+  //   ),
+  //   new Recipe(
+  //     'Lasagna',
+  //     'Some description for the lasagna',
+  //     'https://www.recetasdesbieta.com/wp-content/uploads/2018/10/lasagna-original.-300x169.jpg',
+  //     [new Ingredient('Pasta', 1), new Ingredient('Tomato', 1)]
+  //   ),
+  // ]
 
   constructor(private slService: ShoppingListService) {}
 
@@ -31,6 +32,11 @@ export class RecipeService {
 
   getRecipe(index: number) {
     return this.recipes[index]
+  }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes
+    this.recipesChanged.next(this.recipes.slice())
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
