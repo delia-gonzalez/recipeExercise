faecef965c8cf483e619dd600067a0d7967c71dc
Delia <dedoblege@gmail.com>
2020-05-01 12:41:13 +0200

    feature module for shopping-list , probably with errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,9 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 
 import { AppComponent } from './app.component'
 import { HeaderComponent } from './header/header.component'
-import { RecipesComponent } from './recipes/recipes.component'
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component'
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component'
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component'
-import { ShoppingListComponent } from './shopping-list/shopping-list.component'
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component'
 import { DropdownDirective } from './shared/dropdown.directive'
 import { ShoppingListService } from './shopping-list/shopping-list.service'
 import { AppRoutingModule } from './app-routing.module'
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component'
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component'
 import { RecipeService } from './recipes/recipe.service'
 import { AuthComponent } from './auth/auth.component'
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component'
@@ -23,20 +15,27 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service'
 import { AlertComponent } from './shared/alert/alert.component'
 import { PlaceholderDirective } from './shared/placeholder/placeholder.directive'
 import { RecipesModule } from './recipes/recipes.module'
+import { ShoppingListModule } from './shopping-list/shopping-list.module'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
     DropdownDirective,
     AuthComponent,
     LoadingSpinnerComponent,
     AlertComponent,
     PlaceholderDirective,
   ],
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule, AppRoutingModule, RecipesModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    RecipesModule,
+    ShoppingListModule,
+  ],
   providers: [
     ShoppingListService,
     RecipeService,
---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core'
+import { ShoppingListComponent } from './shopping-list.component'
+import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component'
+import { RouterModule } from '@angular/router'
+import { FormsModule } from '@angular/forms'
+import { CommonModule } from '@angular/common'
+
+@NgModule({
+  declarations: [ShoppingListComponent, ShoppingEditComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([{ path: 'shopping-list', component: ShoppingListComponent }]),
+  ],
+})
+export class ShoppingListModule {}
